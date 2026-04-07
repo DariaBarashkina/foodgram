a92c30ddbac78c460e3b308a2c8df2e8e371e053
Daria Barashkina <barashkinada@gmail.com>
2026-04-07 15:51:21 +0700

    pep8

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Sum
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render
 
 from django_filters.rest_framework import DjangoFilterBackend
 
