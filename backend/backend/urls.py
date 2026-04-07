2ef5c770251646c733b765db103c2e5414a71633
Daria Barashkina <barashkinada@gmail.com>
2026-04-07 18:45:55 +0700

    pep8

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.shortcuts import redirect, get_object_or_404
 from recipes.models import Recipe
-
-from api.views import index
-
 
 def redirect_to_recipe(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
@@ -17,7 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('s/<int:pk>/', redirect_to_recipe, name='short-link'),
-#    re_path(r'^(?!api|admin|static|media|s).*$', index, name='spa-fallback'),
 ]
 
 if settings.DEBUG:
