64400dbd30403819bd58d2cf71693c5a41983ab3
Daria Barashkina <barashkinada@gmail.com>
2026-04-08 22:15:14 +0700

    urls editting

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,7 +8,7 @@ from recipes.models import Recipe
 
 def redirect_to_recipe(request, code):
     recipe = get_object_or_404(Recipe, short_code=code)
-    return redirect(f'/recipes/{recipe.id}/')
+    return redirect(f'/recipes/{recipe.id}')
 
 
 def redirect_to_edit(request, pk):
