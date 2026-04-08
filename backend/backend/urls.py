a29acf726c12935623bb3883f821963488419a14
Daria Barashkina <barashkinada@gmail.com>
2026-04-08 22:25:44 +0700

    pep8

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.shortcuts import redirect, get_object_or_404
 from recipes.models import Recipe
 
