37ac9ebad3ad421f730db70eac08551c731c9e65
Daria Barashkina <barashkinada@gmail.com>
2026-04-19 14:45:27 +0700

    PEP8

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-ы
+
 from recipes.models import (
     Favorite,
     Ingredient,
